6f20e5ffa370fa305fcc970dac883ca73f4ef7c9
Andrew Luko <andrew.w.lukosevic@gmail.com>
2018-01-10 19:05:30 -0500

    Update alexa_startup_dev.sh

--- alexa_startup_dev.sh
+++ alexa_startup_dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Sleep times may vary for your hardware and distribution.
+#This is the core script which runs at startup and calls the other scrips in the repository
 
 lxterminal -t "NPM" --working-directory="/home/pi/Desktop/alexa-avs-sample-app/samples/companionService" -e "./npm_start.sh" &
 sleep 15 
