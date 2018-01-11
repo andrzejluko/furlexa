d83c6a933861173cb657ab4cebaba382de6af078
Andrew Luko <andrew.w.lukosevic@gmail.com>
2018-01-10 19:09:34 -0500

    Create mvn_exec.sh

---
+++ mvn_exec.sh
@@ -0,0 +1,4 @@
+# This file needs to be located in the directory "/home/pi/Desktop/alexa-avs-sample-app/samples/javaclient". 
+# This is the second script to be called in the alexa service startup process. 
+
+mvn exec:exec
