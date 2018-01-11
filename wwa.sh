c5f3cc9410b34887c1c7c87998658893671c248f
Andrew Luko <andrew.w.lukosevic@gmail.com>
2018-01-10 19:11:22 -0500

    Create wwa.sh

---
+++ wwa.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This is was most difficult of the three commands to start the alexa service. 
+# This command runs the WakeWordAgent that allows the furlexa (or alexa) to hear and respond to your voice
+
+cd ~/Desktop/alexa-avs-sample-app/samples/wakeWordAgent/src
+./wakeWordAgent -e kitt_ai
