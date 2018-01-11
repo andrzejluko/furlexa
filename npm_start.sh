7b80362cb883d61d6b1997e673e561b6660f95c3
Andrew Luko <andrew.w.lukosevic@gmail.com>
2018-01-10 19:08:21 -0500

    Create npm_start.sh

---
+++ npm_start.sh
@@ -0,0 +1,4 @@
+# This file needed to be located in the directory "/home/pi/Desktop/alexa-avs-sample-app/samples/companionService".
+# The script alexa_startup_dev.sh calls this first which begins the boot process for the alexa service
+
+npm start
